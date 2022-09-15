d87553bbb828347923f95e124e4cf905c67ff172
Tarek Khaled <tarekkhalid78@gmail.com>
2022-09-15 07:10:40 +0200

    task3 first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,9 @@ fun main() {
     println("Insert Car Type BMW or Tesla")
 
     val car1 = BMW("2009", "Green", 120)
-    val car2 = Tesla("2020", "Red", 130)
-    println("Car 1 is ${car1.getFuleType()}")
-    println("Car 2 is ${car2.getFuleType()}")
+    val car2 = Tesla("2020", "Red", 150)
+    println("BMW is ${car1.getFuleType()}")
+    println("Tesla is ${car2.getFuleType()}")
 }
 
 abstract class Car(model: String, color: String, speed: Int) {
